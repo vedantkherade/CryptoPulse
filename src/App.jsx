36ad2688f6841ad990bbab027bc9b3b5ac40862b
vedantkherade <vedantkherade76@gmail.com>
2026-02-14 00:12:48 +0530

    completed folder structure

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/Common/Header/Header'
+import Footer from './components/Common/Footer/Footer'
+
+
+function App() {
+ 
+
+  return (
+    <>
+    <Header/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
